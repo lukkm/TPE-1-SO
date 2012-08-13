f8e8f519a644b13c78837287245e038c35172b70
Lucas  Moscovicz <lmoscovi@lab114.it.itba.edu.ar>
2012-08-13 19:41:04 -0300

    Cambiado server

--- server.c
+++ server.c
@@ -16,6 +16,8 @@ process_t endif_process;
 process_t while_process;
 process_t endwhile_process;
 
+void init(void);
+
 int
 main(void) 
 {
@@ -52,4 +54,27 @@ main(void)
 		
 
 	return 0;
+}
+
+void init(){
+
+	inc_process = calloc(sizeof(struct process));
+	dec_process = calloc(sizeof(struct process));
+	mr_process = calloc(sizeof(struct process));
+	ml_process = calloc(sizeof(struct process));
+	cz_process = calloc(sizeof(struct process));
+	if_process = calloc(sizeof(struct process));
+	endif_process = calloc(sizeof(struct process));
+	while_process = calloc(sizeof(struct process));
+	endwhile_process = calloc(sizeof(struct process));
+
+	inc_process->type = INC;
+	dec_process->type = DEC;
+	mr_process->type = MR;
+	ml_process->type = ML;
+	cz_process->type = CZ;
+	if_process->type = IF;
+	endif_process->type = ENDIF;
+	while_process->type = WHILE;	
+	endwhile_process->type = ENDWHILE;	
 }
