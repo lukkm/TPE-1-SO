45832654c01826ac1374816808bab48f09fbe0cd
Juan Jose Marinelli <jmarinell@lab110.it.itba.edu.ar>
2012-08-22 18:53:56 -0300

    actualizados cz y endif

--- Processes/endif_process.c
+++ Processes/endif_process.c
@@ -1,49 +1,48 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <semaphore.h>
 #include "../structs.h"
 #include "../defs.h"
+#include "../IPCS/ipcs.h"
 
-int pre_execute(process_params_t);
+process_params_t pre_execute(status_t, int);
+void call_next_process(status, ipc_params_t);
 void* execute_endif (void*);
 void init_processes(void);
 
-process_t endif_process;
+process_t inc_process;
 
-int pre_execute(process_params_t params)
-{
-  
-	pthread_t thread_id;	
-	process_params_t thread_args = params;
-	//pthread_create(&thread_id, NULL, &execute_endif, &thread_args);
-	return 0;
-
-}
-
-
+sem_t sem;
 
 int main(void)
 {
-	char line[100];	
+	status c_program;
+	graph_t mem;
+	process_params_t thread_args;
+	pthread_t thread_id;
+
 	init_processes();
-	printf("Test endif \n");
-	ipc_open(endif_process->params, O_RDONLY|O_NONBLOCK);
+	sem_init(&sem,0,0);
+	ipc_open(endif_process->params, O_RDONLY);
 	while(1){
-		if (ipc_receive(endif_process->params, line, 100) > 0){
-			printf("El proceso endif recibio: %s\n", line);
+		if (ipc_receive(endif_process->params, &c_program, sizeof(struct status)) > 0){ 
+			if ( (mem = (graph_t)shmat(c_program.g_header.fd, c_program.g_header.mem_adress, 0)) == -1 )
+				fatal("shmat");
+			thread_args = pre_execute(&c_program, mem->current->instruction_process->param);
+			printf("endif process\n");			
+			printf("Estado ANTES: %d\n", c_program.mem[c_program.cursor]); 
+			printf("Estado DESPUES: %d\n", c_program.mem[c_program.cursor]); 
+			mem->current = mem->current->true_node;
+			if (mem->current != NULL)
+				call_next_process(c_program, mem->current->instruction_process->instruction_type->params);
+			shmdt(mem);
 		}
 		sleep(1);
 	}
 	
 	return 0;
-}
-
-
-void* execute_endif (void* structure_params)
-{
-	process_params_t par = (process_params_t) structure_params;
-	
-	printf("WTF \n");
-	return NULL;
 }
